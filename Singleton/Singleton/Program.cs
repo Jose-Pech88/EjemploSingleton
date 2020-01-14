73538c12188a187a7ecd60f63dbd52d23478c4bd
José Francisco Pech Bacab <jose.pech@blueoceantech.com.mx>
2020-01-14 08:49:26 -0600

    Versión 1
    Versión 1 del código

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Singleton.Services;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            SingletonExample s1 = SingletonExample.GetInstance();
+            SingletonExample s2 = SingletonExample.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
